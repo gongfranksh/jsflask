b7bcf8f954267d45b8b9f1ff26a7721d9865a53c
weiliang <weiliang@buynow.com.cn>
2019-11-29 14:15:53 +0800

    init

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app.create_app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='192.168.81.148', debug=True)
+    # app.run(host='192.168.81.148', debug=True)
+    app.run(host='0.0.0.0', debug=True)
