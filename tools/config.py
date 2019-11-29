0f74cfead5bf009ed6558fa4051bf1c1982511f5
weiliang <weiliang@buynow.com.cn>
2019-11-29 16:48:11 +0800

    init

--- tools/config.py
+++ tools/config.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+#测卷环境
 js_host = "192.168.72.30"
 js_user = "sa"
 js_pwd = "permain"
@@ -5,7 +8,7 @@ js_db = "headquarters"
 
 
 rt_ok=200
-
+#正式环境
 # js_host = "192.168.72.5"
 # # js_user = "syzy"
 # # js_pwd = "7fad69fa0c"
